db720ef577b0905fc783dabe7fbd854a255f4321
emre gok <46620582+mremregok@users.noreply.github.com>
2023-12-12 14:08:29 +0300

    Revert "re-create"
    This reverts commit a1ada43a3c7dae517db6488411ece6cb288d40f9.

---
+++ Project1/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Project1.Models;
+
+namespace Project1.Controllers
+{
+	public class HomeController : Controller
+	{
+		public List<Expense> Index()
+		{
+			List<Expense> expenses = new List<Expense>
+			{
+				new Expense
+				{
+					Id = "e1",
+					Title = "Toilet Paper",
+					Amount = 94.12,
+					Date = new DateTime(2020, 7, 14),
+				},
+				new Expense
+				{
+					Id = "e2",
+					Title = "New TV",
+					Amount = 799.49,
+					Date = new DateTime(2021, 2, 12),
+				},
+				new Expense
+				{
+					Id = "e3",
+					Title = "Car Insurance",
+					Amount = 294.67,
+					Date = new DateTime(2021, 2, 28),
+				},
+				new Expense
+				{
+					Id = "e4",
+					Title = "New Desk (Wooden)",
+					Amount = 450,
+					Date = new DateTime(2021, 5, 12),
+				},
+			};
+
+			return expenses;
+		}
+	}
+}
---
+++ Project1/Models/Expense.cs
@@ -0,0 +1,10 @@
+﻿namespace Project1.Models
+{
+	public class Expense
+	{
+		public string Id { get; set; }
+		public string Title { get; set; }
+		public double Amount { get; set; }
+		public DateTime Date { get; set; }
+	}
+}
